e58479822a91c16dde3a35e53775f8bd05058613
Holland Graves <zacharyledoux13@gmail.com>
2018-11-19 10:35:08 -0700

    finished ex1, started ex2

---
+++ 001_review_exercises/exercise-2/main.go
@@ -0,0 +1,55 @@
+package main
+
+/*
+- create a struct that holds person fields
+- create a struct that holds secret agent fields and embeds person type
+- attach a method to person: pSpeak
+- attach a method to secret agent: saSpeak
+- create a variable of type person
+- create a variable of type secret agent
+- print a field from person
+- run pSpeak attached to the variable of type person
+- print a field from secret agent
+- run saSpeak attached to the variable of type secret agent
+- run pSpeak attached to the variable of type secret agent
+*/
+
+import "fmt"
+
+// //////////////////////////////////////////////////////////////////////////////////
+// DATA DECLARATIONS
+// //////////////////////////////////////////////////////////////////////////////////
+
+type person struct {
+	fName string
+	lName string
+}
+
+type secretAgent struct {
+	person
+}
+
+// //////////////////////////////////////////////////////////////////////////////////
+// PACKAGE FUNCTIONS
+// //////////////////////////////////////////////////////////////////////////////////
+
+func (p *person) pSpeak() {
+	fmt.Println(p.fName)
+}
+
+func (sa *secretAgent) saSpeak() {
+
+}
+
+// //////////////////////////////////////////////////////////////////////////////////
+// MAIN FUNCTION
+// //////////////////////////////////////////////////////////////////////////////////
+
+func main() {
+	dndChar := person{fName: "Nas", lName: "Lókë"}
+	// saDndChar := secretAgent{
+	// 	person{fName: "Rogar", lName: "The Rogue"},
+	// }
+
+	dndChar.pSpeak()
+}
