0582ca4e57a464db919b5d24495734aff0d35c39
Holland Graves <zacharyledoux13@gmail.com>
2018-11-22 17:53:13 -0700

    finished exercise 5

--- 001_review_exercises/exercise-5/main.go
+++ 001_review_exercises/exercise-5/main.go
@@ -2,7 +2,7 @@ package main
 
 /*
 - Create a new type called person which is STRUCT that stores fName and lName
-- Using the STRUCT “person”, using a composite literal create a value of type person and assign it to a variable with the identifier “p1”
+- Using the STRUCT “person” and a composite literal create a value of type person and assign it to a variable with the identifier “p1”
 - print out “p1”
 - print out just the field fName for “p1”
 - Take the STRUCT “person” and add a field called “favFood” that stores a slice of string
@@ -13,6 +13,51 @@ package main
 - Have the func return this string: <person’s first name> is walking
 */
 
+import "fmt"
+
+// //////////////////////////////////////////////////////////////////////////////////
+// DATA DECLARATIONS
+// //////////////////////////////////////////////////////////////////////////////////
+
+type person struct {
+	fName   string
+	lName   string
+	favFood []string
+}
+
+// //////////////////////////////////////////////////////////////////////////////////
+// PACKAGE FUNCTIONS
+// //////////////////////////////////////////////////////////////////////////////////
+
+func (p *person) walk() {
+	fmt.Printf("%s is walking\n", p.fName)
+
+	return
+}
+
+// //////////////////////////////////////////////////////////////////////////////////
+// MAIN FUNCTION
+// //////////////////////////////////////////////////////////////////////////////////
+
 func main() {
+	p1 := person{
+		fName: "Nas",
+		lName: "Lóckë",
+		favFood: []string{
+			"Ramen",
+			"Pork",
+			"Kombucha",
+		},
+	}
+
+	fmt.Println(p1)
+	fmt.Println(p1.fName)
+	fmt.Println(p1.favFood)
+
+	for _, food := range p1.favFood {
+		fmt.Println(food)
+	}
+
+	p1.walk()
 
 }
