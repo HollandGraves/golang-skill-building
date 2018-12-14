ae26fa160fa9004a60da716dc57229de04579301
Holland Graves <zacharyledoux13@gmail.com>
2018-12-13 21:45:07 -0700

    noticed an error at compile when attempting to compile a function with two return values when one wasn't of type err. Commented out the function and noted this fact for furture onlooking.

--- 002_templates/04_passing-functions-into-templates/2_Pipelines/main.go
+++ 002_templates/04_passing-functions-into-templates/2_Pipelines/main.go
@@ -17,9 +17,9 @@ import (
 // //////////////////////////////////////////////////////////////////////////////////
 
 var fm = template.FuncMap{
-	"di":   DoubleInt,
-	"sn":   SquareNum,
-	"nitf": NumIntToFloat64,
+	"di": DoubleInt,
+	"sn": SquareNum,
+	// "nitf": NumIntToFloat64, this function wont work for reasons below
 }
 
 var tpl *template.Template
@@ -37,10 +37,16 @@ func DoubleInt(x int) int {
 	return x * 2
 }
 
-// NumIntToFloat64 : turns an int into float64 type
-func NumIntToFloat64(x int) float64 {
-	return float64(x)
-}
+// This function will not be able to be passed because it
+// returns two values and one of the values types is not of
+// type err.
+// The only functions that can compile into templates are
+// functions that have one return value, or two return values
+// with one being of type err and the other being of another type
+//
+// func NumIntToFloat64(x int, y int) (float64, float64) {
+// 	return float64(x), float64(y)
+// }
 
 // SquareNum : well, it squares num!
 func SquareNum(x float64, y float64) float64 {
